9caf9d5ca731b313b96cf722c53965ebf96d0149
jacobknows2much <jacobknows2much@gmail.com>
2013-10-23 22:58:29 -0500

    dfsgg

---
+++ helo.py
@@ -0,0 +1,33 @@
+hello= raw_input('You wake up to a wonderful day. You get out bed and run to the couch. You grab the remote to the tv and push the power button."what has happened theres no connection and look theres no phone service." what should I do should I go outside.')
+
+if hello== "yes":
+    what= raw_input('what is this? whats happening? where am i going? is that a black hole? whyyyyyyyyyyyyyyy oh noooooooooooo')
+else:
+    no= raw_input('I guess we can stay here then. Your door flies off and you fly out the door into a black hole looking thing')
+
+oh= raw_input('You fall into a pit  and  wonder why i'm here and what happened and "who am I?"')
+who= raw_input("Who am I?")
+print "oh ok so I'm",who
+to= raw_input ('I need to find my way out of here ummmmmm "where should i go?"')
+whu= raw_input('where should I go? "left" or "right"')
+
+if whu== "left":
+    diamand= raw_input("on the left side you find nothing but a diamond  so you head on to the right looking for more rewards ")
+right= raw_input('you find a "road" and a "trail" where will you search')
+
+if right== "road":
+    road= raw_input('along the road you find nothing interesting so you go onto the trail')
+trail= raw_input('the trail has many interesting things but two things catch your eye a "golden shovel" and a "iron sword"')
+
+if trail== "golden shovel":
+    shovel= raw_input(' You grab the shovel and decide to dig something what should you dig the "dirt" or the "sand" in the distance')
+elif trail== "iron sword":
+    sword= raw_input('after finding the sword you find a pickaxe which seems of no use so you head to a cave nearby ‘) 
+
+if sword== "proceed":
+    proceed= raw_input('you proceed into the cave nearby')
+elif sword== "mine":
+    mine= raw_input('you mine then find a fire staff and head into a cave where your fate lies')
+
+print who, “are you ready for what lies ahead”
+
